8539c01c6a4ad52ccf589c487385d0640fca3742
Abdulrahmannas <a.nabbout@fromscratch-solutions.com>
2024-06-20 18:01:07 +0300

    updates

--- app/Console/Commands/TrackCommand.php
+++ app/Console/Commands/TrackCommand.php
@@ -7,27 +7,45 @@ use Illuminate\Console\Command;
 
 class TrackCommand extends Command
 {
-    /**
-     * The name and signature of the console command.
-     *
-     * @var string
-     */
+
     protected $signature = 'track';
 
-    /**
-     * The console command description.
-     *
-     * @var string
-     */
     protected $description = 'Track all product stock';
 
-    /**
-     * Execute the console command.
-     */
     public function handle()
     {
-        Product::all()->each->track();
+        // $bar = $this->output->createProgressBar();
+        // $bar->start();
+        // $this->output->progressStart($products->count());
+
+        Product::all()
+            ->tap(fn($products) => $this->output->progressStart($products->count()))
+            ->each(function ($product) {
+                $product->track();
+                $this->output->progressAdvance();
+            });
+
+        $this->output->progressFinish();
+
+        $this->showResults();
 
         $this->info('All done!');
+    }
+
+    protected function showResults()
+    {
+        $data = Product::query()
+            ->leftJoin('stocks', 'stocks.product_id', '=', 'products.id')
+            ->get($this->tableKeys());
+
+        $this->table(
+            array_map('ucwords', $this->tableKeys()),
+            $data
+        );
+    }
+
+    public function tableKeys(): array
+    {
+        return ['name', 'price', 'url', 'in_stock'];
     }
 }
